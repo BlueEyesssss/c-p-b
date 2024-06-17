d1443527ad79ab3ece5d5f81117b2bc353d22f67
Pham Huynh Phuong Kha <vuamessi9@gmail.com>
2024-06-17 15:25:59 +0700

    new code

--- Changepass/jsa.js
+++ Changepass/jsa.js
@@ -1,10 +1,22 @@
-function Changee(){
+function ChangeeCode(){
     var inputt = document.getElementById("inputt");
     var outputt = document.getElementById("resultt");
     var s = "";
     if(document.getElementById("sm").onclick){
         for (let i = 0; i < inputt.value.length; i++) {
             var a = inputt.value.charCodeAt(i) - 1;
+            s = s.concat(String.fromCharCode(a));
+        }
+        outputt.value = s;
+    }
+}
+function ChangeePass(){
+    var inputt = document.getElementById("inputt1");
+    var outputt = document.getElementById("resultt1");
+    var s = "";
+    if(document.getElementById("sm1").onclick){
+        for (let i = 0; i < inputt.value.length; i++) {
+            var a = inputt.value.charCodeAt(i) + 1;
             s = s.concat(String.fromCharCode(a));
         }
         outputt.value = s;
